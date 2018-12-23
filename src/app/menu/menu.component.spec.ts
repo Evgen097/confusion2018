bbff866e5a2b584fc1cd516e5582acf72a25bf59
Evgen097 <k0990245853@gmail.com>
2018-12-23 19:39:43 +0200

    add testing

--- src/app/menu/menu.component.spec.ts
+++ src/app/menu/menu.component.spec.ts
@@ -1,16 +1,57 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { MenuComponent } from './menu.component';
+import { RouterTestingModule } from '@angular/router/testing';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+// import { MaterialModule } from '@angular/material';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { Dish } from '../shared/dish';
+import { DishService } from '../services/dish.service';
+import { DISHES } from '../shared/dishes';
+import { baseURL } from '../shared/baseurl';
+import { Observable } from 'rxjs/Observable';
+
+
+import { MatAutocompleteModule, MatButtonModule, MatButtonToggleModule, MatCardModule, MatCheckboxModule, MatChipsModule, MatDatepickerModule, MatDialogModule, MatExpansionModule, MatGridListModule, MatIconModule, MatInputModule, MatListModule, MatMenuModule, MatNativeDateModule, MatPaginatorModule, MatProgressBarModule, MatProgressSpinnerModule, MatRadioModule, MatRippleModule, MatSelectModule, MatSidenavModule, MatSliderModule, MatSlideToggleModule, MatSnackBarModule, MatSortModule, MatTableModule, MatTabsModule, MatToolbarModule, MatTooltipModule } from '@angular/material';
+
+
+
+import { By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
+
+import { of } from 'rxjs';
 
 describe('MenuComponent', () => {
   let component: MenuComponent;
   let fixture: ComponentFixture<MenuComponent>;
 
   beforeEach(async(() => {
+
+    let dishServiceStub = {
+      getDishes: function(): Observable<Dish[]> {
+        return of(DISHES);
+      }
+    };
+
     TestBed.configureTestingModule({
-      declarations: [ MenuComponent ]
+      imports: [ BrowserAnimationsModule,
+        FlexLayoutModule,
+        RouterTestingModule.withRoutes([{ path: 'menu', component: MenuComponent }]),
+
+        MatAutocompleteModule, MatButtonModule, MatButtonToggleModule, MatCardModule, MatCheckboxModule, MatChipsModule, MatDatepickerModule, MatDialogModule, MatExpansionModule, MatGridListModule, MatIconModule, MatInputModule, MatListModule, MatMenuModule, MatNativeDateModule, MatPaginatorModule, MatProgressBarModule, MatProgressSpinnerModule, MatRadioModule, MatRippleModule, MatSelectModule, MatSidenavModule, MatSliderModule, MatSlideToggleModule, MatSnackBarModule, MatSortModule, MatTableModule, MatTabsModule, MatToolbarModule, MatTooltipModule,
+
+
+      ],
+      declarations: [ MenuComponent ],
+      providers: [
+        { provide: DishService, useValue: dishServiceStub },
+        { provide: 'BaseURL', useValue: baseURL },
+      ]
     })
-    .compileComponents();
+      .compileComponents();
+
+    let dishservice = TestBed.get(DishService);
+
   }));
 
   beforeEach(() => {
@@ -19,7 +60,28 @@ describe('MenuComponent', () => {
     fixture.detectChanges();
   });
 
+
+
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('dishes items should be 4', () => {
+    expect(component.dishes.length).toBe(4);
+    expect(component.dishes[1].name).toBe('Zucchipakoda');
+    expect(component.dishes[3].featured).toBeFalsy();
+  });
+
+  it('should use dishes in the template', () => {
+    fixture.detectChanges();
+
+    let de:      DebugElement;
+    let el:      HTMLElement;
+    de = fixture.debugElement.query(By.css('h1'));
+    el = de.nativeElement;
+
+    expect(el.textContent).toContain(DISHES[0].name.toUpperCase());
+
+  });
+
 });
